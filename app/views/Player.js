b275fab13da65d97acd0e00bb8d183d06786af9d
xavi-mat <1102677+xavi-mat@users.noreply.github.com>
2023-12-11 00:33:41 +0100

    getting video url

--- app/views/Player.js
+++ app/views/Player.js
@@ -1,38 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-const styles = StyleSheet.create({
-    contenedor: {
-        flex: 1,
-    },
-    titulo: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    lugar: {
-        flex: 3,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-});
+import { Text, View, Image } from 'react-native';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import { styles } from '../styles/styles';
 
 export class Player extends React.Component {
 
     state = {
         video_resumen: '',
-    }
+        videoPath: '',
+    };
 
     componentDidMount() {
         const { video_resumen } = this.props.route.params;
         this.setState({ video_resumen });
+
+        // Obtenemos de firebase la URL completa del vídeo
+        const storage = getStorage();
+        const storageRef = ref(storage, video_resumen);
+        getDownloadURL(storageRef)
+            .then(url=>{
+                console.log("URL: ", url);
+                this.setState({ videoPath: url });
+            })
+    }
+
+    VideoBox = () => {
+        return (
+            <View style={styles.videoBox}>
+                <Text>VIDEOBOX, aquí la url: {this.state.videoPath}</Text>
+                <Image
+                    source={{
+                        uri: this.state.videoPath,
+                        method: 'GET'
+                     }}
+                    style={styles.video}
+                />
+            </View>
+        );
     }
 
     render() {
+
+        if (this.state.videoPath === '') {
+            return (
+                <View style={styles.container}>
+                    <View style={styles.loader}>
+                        <Text>Cargando...</Text>
+                    </View>
+                </View>
+            );
+        }
+
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>AQUÍ EL REPRODUCTOR</Text>
                 <Text>He recibido esta string: {this.state.video_resumen}</Text>
+                <this.VideoBox />
                 <Text>BOTÓN 1</Text>
                 <Text>BOTÓN 2</Text>
                 <Text>BOTÓN 3</Text>
