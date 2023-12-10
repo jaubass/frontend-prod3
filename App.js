c0d91ce0ff73f972e39a9771381f2ee5b9c56cc1
xavi-mat <1102677+xavi-mat@users.noreply.github.com>
2023-12-10 13:55:35 +0100

    volver al estado anterior, antes de navigation

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home } from './app/views/Home';
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Home />
   );
 }
