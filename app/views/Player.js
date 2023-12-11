d3e2e68e661292ebf93944664555d365bbc58888
xavi-mat <1102677+xavi-mat@users.noreply.github.com>
2023-12-11 01:09:35 +0100

    Video with expo

--- app/views/Player.js
+++ app/views/Player.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View } from 'react-native';
 import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import { Video, ResizeMode } from 'expo-av';
+
 import { styles } from '../styles/styles';
 
 export class Player extends React.Component {
 
+    // video = React.useRef(null);
     state = {
         video_resumen: '',
         videoPath: '',
@@ -18,26 +21,36 @@ export class Player extends React.Component {
         const storage = getStorage();
         const storageRef = ref(storage, video_resumen);
         getDownloadURL(storageRef)
-            .then(url=>{
+            .then(url => {
                 console.log("URL: ", url);
                 this.setState({ videoPath: url });
             })
     }
 
-    VideoBox = () => {
-        return (
-            <View style={styles.videoBox}>
-                <Text>VIDEOBOX, aquí la url: {this.state.videoPath}</Text>
-                <Image
-                    source={{
-                        uri: this.state.videoPath,
-                        method: 'GET'
-                     }}
-                    style={styles.video}
-                />
-            </View>
-        );
-    }
+    // VideoBox = () => {
+    //     return (
+    //         <View style={styles.videoBox}>
+    //             <Text>VIDEOBOX, aquí la url: {this.state.videoPath}</Text>
+    //             {/* <Image
+    //                 source={{
+    //                     uri: this.state.videoPath,
+    //                     method: 'GET'
+    //                  }}
+    //                 style={styles.video}
+    //             /> */}
+    //             <Video
+    //                 // source={{ uri: this.state.videoPath }}
+    //                 source={{ url: "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4" }}
+
+    //                 // ref={(ref) => { this.player = ref }}
+    //                 controls={true}
+    //                 // onBuffer={this.onBuffer}                // Callback when remote video is buffering
+    //                 // onError={this.videoError}               // Callback when video cannot be loaded
+    //                 style={styles.video}
+    //             />
+    //         </View>
+    //     );
+    // }
 
     render() {
 
@@ -47,6 +60,7 @@ export class Player extends React.Component {
                     <View style={styles.loader}>
                         <Text>Cargando...</Text>
                     </View>
+
                 </View>
             );
         }
@@ -55,7 +69,21 @@ export class Player extends React.Component {
             <View style={styles.container}>
                 <Text>AQUÍ EL REPRODUCTOR</Text>
                 <Text>He recibido esta string: {this.state.video_resumen}</Text>
-                <this.VideoBox />
+                <View style={styles.videoBox}>
+                    <Text>VIDEOBOX, aquí la url: {this.state.videoPath}</Text>
+                    {/* <Video
+                        source={{ uri: this.state.videoPath }}
+                        controls={true}
+                        style={styles.video}
+                    /> */}
+                    <Video
+                        // ref={this.video}
+                        style={styles.video}
+                        source={{ uri: this.state.videoPath }}
+                        useNativeControls
+                        resizeMode={ResizeMode.CONTAIN}
+                    />
+                </View>
                 <Text>BOTÓN 1</Text>
                 <Text>BOTÓN 2</Text>
                 <Text>BOTÓN 3</Text>
